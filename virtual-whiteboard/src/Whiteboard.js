01d50195bcfb0f266d7ca3ad0396da303cefebfc
Jonathan Hertz <jonathan_hertz2@live.dk>
2021-10-15 12:26:44 +0200

    Textbox now data driven

--- virtual-whiteboard/src/Whiteboard.js
+++ virtual-whiteboard/src/Whiteboard.js
@@ -2,7 +2,43 @@ import { Component } from 'react';
 import { Textbox } from './Textbox';
 
 export class Whiteboard extends Component {
+    // Data
+    // -Type (Textbox, Link, Image, Video)
+    // -Data (Depends on type)
+    // -Position
+    state = {
+        // Can be retrieved from backend when built
+        entries: [
+            {
+                type: "textbox",
+                position: {
+                    x: 50,
+                    y: 100
+                },
+                data: {
+                    text: "Hello"
+                }
+            },
+            {
+                type: "textbox",
+                position: {
+                    x: 100,
+                    y: 50
+                },
+                data: {
+                    text: "World"
+                }
+            }
+        ]
+    }
     render() {
-        return <Textbox text="Test" x="50" y="100"/>;
+        let c = this.state.entries.map(entry => {
+            switch (entry.type) {
+                case "textbox":
+                    return (<Textbox text={entry.data.text} x={entry.position.x} y={entry.position.y}/>)
+                    break;
+            }
+        })
+        return c;
     }
 }
