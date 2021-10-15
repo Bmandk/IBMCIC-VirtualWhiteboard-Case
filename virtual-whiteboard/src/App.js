5fafd5f5c8da5deeb63cd903941709734fd7f30e
Jonathan Hertz <jonathan_hertz2@live.dk>
2021-10-15 12:03:50 +0200

    Initialize React

---
+++ virtual-whiteboard/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import { Whiteboard } from './Whiteboard'
+import './App.css';
+
+function App() {
+  return (
+    <Whiteboard></Whiteboard>
+  );
+}
+
+export default App;
